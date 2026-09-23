ced3e2595ce829d5e5e725c684443361f0cb3a00
Devin Rafa Haryanto <devinrafa7@gmail.com>
2026-09-23 21:58:50 +0700

    add: primefinder step menu 1

--- ch05-ch06/primeFinderTriangleMenu.c
+++ ch05-ch06/primeFinderTriangleMenu.c
@@ -1,15 +1,54 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
-    int n=0;
+    int inputMenu=0, a=0, b=0, divider, cPrime=0, n=0;
+    bool isPrime; 
+
     do{
-        printf("=== MENU ===");
-        printf("1. Find the prime number in range [a, b]");
-        printf("2. Print triangle numbers pattern");
-        printf("3. Exit");
+        printf("=== MENU ===\n");
+        printf("1. Find the prime number in range [a, b]\n");
+        printf("2. Print triangle numbers pattern\n");
+        printf("3. Exit\n");
+
+        printf("input the menu with number 1-3: ");
+        scanf("%d", &inputMenu);
 
 
-    }while(n!=3);
+        switch (inputMenu)
+        {
+        case 1:
+            do{
+                printf("input a, and b (a<=b): ");
+                scanf("%d %d", &a, &b);
+            }while(a>b);
+
+            printf("\nPrime found: ");
+
+            for (n=a; n<=b; n++){
+                isPrime = true;
+                divider=2;
+
+                do{
+                    if(isPrime){
+                        isPrime = (n%divider == 0) ? false : true;
+                        divider += (isPrime) ? 1 : 0;
+                    }
+                    if (divider == n-1){
+                        printf("%d ", n);
+                        cPrime++;
+                    }
+                }while(divider<(n-1) && isPrime);    
+            }
+
+            printf("\nTotal prime in range: %d \n\n", cPrime);
+            break;
+        
+        default:
+            break;
+        }
+
+    }while(inputMenu!=3);
 
     return 0;
 }
