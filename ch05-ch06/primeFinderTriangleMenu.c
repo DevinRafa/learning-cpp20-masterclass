773558dd0495b65856580dc45e37efbf18ec7bf5
Devin Rafa Haryanto <devinrafa7@gmail.com>
2026-09-23 14:14:41 +0700

    add: prime finder

---
+++ ch05-ch06/primeFinderTriangleMenu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n=0;
+    do{
+        printf("=== MENU ===");
+        printf("1. Find the prime number in range [a, b]");
+        printf("2. Print triangle numbers pattern");
+        printf("3. Exit");
+
+
+    }while(n!=3);
+
+    return 0;
+}
