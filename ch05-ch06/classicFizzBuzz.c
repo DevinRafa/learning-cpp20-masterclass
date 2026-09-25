cd6ed2637fe579fe32d2eb0394a149db19d98650
Devin Rafa Haryanto <devinrafa7@gmail.com>
2026-09-25 14:39:11 +0700

    fix: iterator initializing value to 1, else if bug to i, data type fix wierd integral types between int and size_t

--- ch05-ch06/classicFizzBuzz.c
+++ ch05-ch06/classicFizzBuzz.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    int n = 0;
+    size_t n = 0;
 
     do{
         printf("input n value (1-100): ");
         scanf("%d", &n);
 
         if(n<1 || n>100){
-            printf("input n value by the range that between 1-100 !");
+            printf("input n value by the range that between 1-100 !\n");
         }
     }while(n<1 || n>100);
 
-    for (size_t i=0; i<=n; i++){
+    for (size_t i=1; i<=n; i++){
         if (i%3==0 && i%5==00){
             printf("FizzBuzz\n");
-            continue;
         }else if (i%3==0){
             printf("Fizz\n");
-            continue;
-        }else if (1%5==0){
+        }else if (i%5==0){
             printf("Buzz\n");
-            continue;
         }else{
-            printf("%d\n", i);
-            continue;
+            printf("%zu\n", i);
         }
     }
 
